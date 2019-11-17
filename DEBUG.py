97950ff779b5034bb63e6f1ffa0478c92c204f2f
BetterWill <45932124+BetterWill@users.noreply.github.com>
2019-11-17 23:11:06 +0800

    2019-11-17 23:06:24 编码错误
    2019-11-17 23:06:24 编码错误

---
+++ DEBUG.py
@@ -0,0 +1,9 @@
+2019-11-17 23:06:24
+  response = requests.get(url,headers=header)
+  response.encoding = 'utf8'
+  print(response.text)
+  doc = open('film1.txt','w')
+  print(response.text,file=doc)
+  doc.close()
+ 出现错误   UnicodeEncodeError: 'gbk' codec can't encode character '\xa9' in position 205104: illegal multibyte sequence
+ 改正：   doc = open('film1.txt','w', ,encoding='utf-8')
